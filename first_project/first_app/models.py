3b3e35bd9a5cc4d236e578b6652179aa64e25474
Alvaro Lloret de Muller <alvaro_lloret@hotmail.com>
2018-05-16 16:26:42 +0200

    Lecture 128: Populating the database with python script, amazing!

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -20,7 +20,6 @@ class Webpage(models.Model):
 class AccesRecord(models.Model):
     name=models.ForeignKey(Webpage, on_delete=models.DO_NOTHING,)
     date=models.DateField()
-    url=models.URLField(unique=True)
 
     def __str__(self):
         return str(self.date)
