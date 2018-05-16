091cc74b1140933d0a78b286534dab5b80cd3a60
Alvaro Lloret de Muller <alvaro_lloret@hotmail.com>
2018-05-16 15:38:35 +0200

    Lecture 127: Creating models in first_app, models=databases. Django takes care once the model is created, to create the database (which can be either MongoDB or SQLLite

--- first_project/first_app/admin.py
+++ first_project/first_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+#lecture 127
+from first_app.models import AccesRecord, Topic, Webpage
 # Register your models here.
+
+#python manage.py createsuperuser
+#username: alvaro_lloret@hotmail.com
+#password: aloa6527
+
+
+admin.site.register(AccesRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
