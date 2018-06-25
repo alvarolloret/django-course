ca1d03475b975e7925f385321e0a038f726940d9
Alvaro Lloret de Muller <alvaro_lloret@hotmail.com>
2018-06-25 20:50:27 +0200

    Not solved problem with second database UserAge. Still, finally /viewuser is finished and looks great!

--- populate_users_app.py
+++ populate_users_app.py
@@ -15,13 +15,11 @@ from faker import Faker
 
 fakegen =Faker()
 topics=['Search', 'Social', 'Marketplace', 'News', 'Games']
-
 def add_name():
     fake_name = fakegen.name()
-    fake_surname = fakegen.name()
     fake_email = fakegen.email()
     fake_age = fakegen.random_int(min=18, max=99)
-    t=User.objects.get_or_create(name=fake_name, surname=fake_surname, email=fake_email, age=fake_age)[0]
+    t=User.objects.get_or_create(name=fake_name, email=fake_email, age=fake_age)[0]
     t.save()
     return t
 
@@ -32,8 +30,8 @@ def populate(N=5):
 
         #create the fake data for that entry
         #Create the new webpage entry
-        usr=UserAge.objects.get_or_create(user_age=top.age, user_name=top.name)[0]
-        #
+        #usr=UserAge.objects.get_or_create(user_age=top.age,user_name=top.name)[0]
+        #user_age=top.age,
         # #Create fqke acces AccesRecord
         # acc_rec =AccesRecord.objects.get_or_create(name=webpg, date=fake_date)[0]
 
--- users_app/models.py
+++ users_app/models.py
@@ -3,11 +3,9 @@ from django.db import models
 # Create your models here.
 #lesson 127
 class User (models.Model):
-    name=models.CharField(max_length=264, unique=True)
-    surname=models.CharField(max_length=264, unique=True)
+    name=models.CharField(max_length=264)
     email=models.EmailField(max_length=264, unique=True)
     age=models.IntegerField(default=0)
-    password=models.IntegerField(default=0)
     def __str__(self):
         return self.name
 
